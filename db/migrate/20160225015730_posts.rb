4c32e00b5cc78979677bb6fa6b9d024fb4a598bd
Kris <weldon.kris@gmail.com>
2016-02-24 21:02:59 -0500

    post database added

---
+++ db/migrate/20160225015730_posts.rb
@@ -0,0 +1,10 @@
+class Posts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.text :message
+      t.datetime :posted, default: Time.now
+    end
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225011353) do
+ActiveRecord::Schema.define(version: 20160225015730) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "message"
+    t.datetime "posted",  default: '2016-02-25 02:01:18'
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
