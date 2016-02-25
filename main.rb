c2bc926f805cd2ac177997e2da8e682edf1e4e13
Brittany Wilson <wilsonbrittm@gmail.com>
2016-02-25 11:29:03 -0500

    bracnch for main ruby skeleton

--- main.rb
+++ main.rb
@@ -1,10 +1,45 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
-
+enable :sessions
 
 set :database, "sqlite3:microblog.sqlite3"
 
 
 require './models/posts'
 require './models/users'
+
+get '/' do
+  current_user
+  erb :fullprofile
+end
+
+get '/feed' do
+  erb :feed
+end
+
+get '/following' do
+  erb :following
+end
+
+get '/sign-in' do
+  @user =User.where(email: params[:email]).first
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @message = "Incorrect Sign-in Information."
+    redirect '/sign-in'
+  end
+  erb :modal
+end
+
+get '/sign-up' do
+  erb :modal
+end
+
+def current_user
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+end
